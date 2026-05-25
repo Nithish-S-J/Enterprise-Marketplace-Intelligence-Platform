5e119dfe4f543176785c0d2e41802ff4356401d6
Nithish_S_J <96019176+Nithish-S-J@users.noreply.github.com>
2026-05-25 21:01:34 +0530

    Update dim_customer.sql

--- sql/warehouse/dimension_tables/dim_customer.sql
+++ sql/warehouse/dimension_tables/dim_customer.sql
@@ -1,28 +1,25 @@
-CREATE TABLE dim_customers
-AS
+DROP TABLE IF EXISTS dbo.dim_customers;
+GO
 
-SELECT
+CREATE TABLE dbo.dim_customers (
+    customer_id VARCHAR(100) NOT NULL,
+    customer_segment VARCHAR(50),
+    customer_type VARCHAR(50)
+);
+GO
+
+INSERT INTO dbo.dim_customers
+SELECT 
     customer_id,
-
-    COUNT(order_id) AS total_orders,
-
-    AVG(order_value_inr) AS avg_order_value,
-
-    AVG(service_rating) AS avg_service_rating,
-
-    COUNT(DISTINCT platform) AS platforms_used,
-
-    CASE
-        WHEN COUNT(order_id) >= 15 THEN 'Frequent'
-        WHEN COUNT(order_id) >= 8 THEN 'Regular'
-        ELSE 'Occasional'
-    END AS customer_order_segment,
-
-    CASE
-        WHEN AVG(service_rating) >= 4 THEN 'Satisfied'
-        ELSE 'Needs Attention'
-    END AS customer_satisfaction_segment
-
-FROM silver_orders
-
+    CASE 
+        WHEN SUM(order_value_inr) > 12000 THEN 'Platinum VIP'
+        WHEN COUNT(order_id) > 10 THEN 'High Frequency'
+        ELSE 'Standard'
+    END AS customer_segment,
+    CASE 
+        WHEN COUNT(order_id) = 1 THEN 'New Customer'
+        ELSE 'Repeat Customer'
+    END AS customer_type
+FROM dbo.silver_orders
 GROUP BY customer_id;
+GO
