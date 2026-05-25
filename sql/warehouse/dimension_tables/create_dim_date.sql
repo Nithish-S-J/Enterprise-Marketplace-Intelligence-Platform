dcf0df33bac332322ea9dcddd2127f4f1baaaf4d
Nithish_S_J <96019176+Nithish-S-J@users.noreply.github.com>
2026-05-25 21:02:39 +0530

    Update create_dim_date.sql

--- sql/warehouse/dimension_tables/create_dim_date.sql
+++ sql/warehouse/dimension_tables/create_dim_date.sql
@@ -1,90 +1,26 @@
--------------------------------------------------------create_dim_date---------------------------------------------------------
+DROP TABLE IF EXISTS dbo.dim_date;
+GO
 
-CREATE TABLE dim_date
-(
-    date_key INT,
-    full_date DATE,
-    year INT,
+CREATE TABLE dbo.dim_date (
+    full_date DATE NOT NULL,
+    [year] INT,
     quarter INT,
-    month INT,
+    [month] INT,
     month_name VARCHAR(20),
-    day_of_month INT,
-    day_name VARCHAR(20),
-    weekday_number INT,
-    weekend_flag VARCHAR(10)
+    weekday VARCHAR(20),
+    week_number INT
 );
+GO
 
-
--------------------------------------------------------------------------------------------------------------------------------------------------------
-#insert below query
-
-
-WITH numbers AS
-(
-    SELECT 0 AS n
-    UNION ALL SELECT 1
-    UNION ALL SELECT 2
-    UNION ALL SELECT 3
-    UNION ALL SELECT 4
-    UNION ALL SELECT 5
-    UNION ALL SELECT 6
-    UNION ALL SELECT 7
-    UNION ALL SELECT 8
-    UNION ALL SELECT 9
-)
-
-INSERT INTO dim_date
-SELECT
-    CAST(FORMAT(DATEADD(DAY,
-        ROW_NUMBER() OVER (ORDER BY a.n) - 1,
-        '2025-01-01'),'yyyyMMdd') AS INT),
-
-    DATEADD(DAY,
-        ROW_NUMBER() OVER (ORDER BY a.n) - 1,
-        '2025-01-01'),
-
-    YEAR(DATEADD(DAY,
-        ROW_NUMBER() OVER (ORDER BY a.n) - 1,
-        '2025-01-01')),
-
-    DATEPART(QUARTER,
-        DATEADD(DAY,
-        ROW_NUMBER() OVER (ORDER BY a.n) - 1,
-        '2025-01-01')),
-
-    MONTH(DATEADD(DAY,
-        ROW_NUMBER() OVER (ORDER BY a.n) - 1,
-        '2025-01-01')),
-
-    DATENAME(MONTH,
-        DATEADD(DAY,
-        ROW_NUMBER() OVER (ORDER BY a.n) - 1,
-        '2025-01-01')),
-
-    DAY(DATEADD(DAY,
-        ROW_NUMBER() OVER (ORDER BY a.n) - 1,
-        '2025-01-01')),
-
-    DATENAME(WEEKDAY,
-        DATEADD(DAY,
-        ROW_NUMBER() OVER (ORDER BY a.n) - 1,
-        '2025-01-01')),
-
-    DATEPART(WEEKDAY,
-        DATEADD(DAY,
-        ROW_NUMBER() OVER (ORDER BY a.n) - 1,
-        '2025-01-01')),
-
-    CASE
-        WHEN DATENAME(WEEKDAY,
-            DATEADD(DAY,
-            ROW_NUMBER() OVER (ORDER BY a.n) - 1,
-            '2025-01-01'))
-        IN ('Saturday','Sunday')
-        THEN 'Weekend'
-        ELSE 'Weekday'
-    END
-
-FROM numbers a
-CROSS JOIN numbers b
-CROSS JOIN numbers c;
+INSERT INTO dbo.dim_date
+SELECT DISTINCT
+    order_date AS full_date,
+    DATEPART(YEAR, order_date) AS [year],
+    DATEPART(QUARTER, order_date) AS quarter,
+    DATEPART(MONTH, order_date) AS [month],
+    DATENAME(MONTH, order_date) AS month_name,
+    DATENAME(WEEKDAY, order_date) AS weekday,
+    DATEPART(WEEK, order_date) AS week_number
+FROM dbo.silver_orders
+WHERE order_date IS NOT NULL;
+GO
