be914f510e47c7ef02736ccdcaac265d19cca8bc
Nithish_S_J <96019176+Nithish-S-J@users.noreply.github.com>
2026-05-29 10:32:31 +0530

    Update dim_restaurants.sql

--- sql/warehouse/dimension_tables/dim_restaurants.sql
+++ sql/warehouse/dimension_tables/dim_restaurants.sql
@@ -1,27 +1,37 @@
 DROP TABLE IF EXISTS dbo.dim_restaurants;
 GO
 
-CREATE TABLE dbo.dim_restaurants (
-    restaurant_id INT NOT NULL,
-    restaurant_name VARCHAR(255) NOT NULL,
-    cuisine_types VARCHAR(255),
-    average_rating DECIMAL(3,2),
+CREATE TABLE dbo.dim_restaurants
+(
+    restaurant_id INT,
+    restaurant_name VARCHAR(200),
+    city VARCHAR(100),
+    area VARCHAR(100),
+    cuisine_types VARCHAR(500),
+    average_rating DECIMAL(5,2),
+    estimated_delivery_time INT,
     restaurant_rating_category VARCHAR(50),
     total_ratings INT,
-    price_tier INT,
+    price INT,
     cost_category VARCHAR(50)
 );
 GO
 
 INSERT INTO dbo.dim_restaurants
-SELECT 
+
+SELECT
+
     restaurant_id,
     restaurant_name,
+    city,
+    area,
     cuisine_types,
     average_rating,
+    estimated_delivery_time,
     restaurant_rating_category,
     total_ratings,
     price,
     cost_category
+
 FROM dbo.silver_restaurants;
 GO
